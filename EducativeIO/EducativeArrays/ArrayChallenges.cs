cf98f251ffc776541b38b1e93975c8fff58d741b
gamerwalt <gamerwalt@gmail.com>
2021-03-23 21:06:06 -0600

    Initial commit

---
+++ EducativeIO/EducativeArrays/ArrayChallenges.cs
@@ -0,0 +1,218 @@
+﻿using System;
+
+namespace EducativeArrays
+{
+    public class ArrayChallenges
+    {
+        public static int FindSecondMaximum(int[] arr)
+        {
+            int first = int.MinValue;
+            int second = first;
+            if (arr.Length < 2) throw new ArgumentException("The arr should contain at least 2 unique elements.");
+
+            for(int i = 0; i<arr.Length; i++)
+            {
+                if(arr[i] > first)
+                {
+                    int temp = first;
+                    first = arr[i];
+                    second = temp;
+                }
+                else
+                {
+                    if(arr[i] > second)
+                    {
+                        second = arr[i];
+                    }
+                }
+            }
+
+            return second;
+        }
+
+        public static int FindFirstUnique(int[] arr)
+        {
+            int currentValue = arr[0];
+            bool found = true;
+            for(int i = 0; i<arr.Length; i++)
+            {
+                currentValue = arr[i];
+                for(int j = 0; j<arr.Length; j++)
+                {
+                    if(currentValue == arr[j] && i != j)
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (found == true) return currentValue;
+                found = true;
+            }
+
+            return currentValue;
+        }
+
+        public static int FindMinimum(int[] arr)
+        {
+            int min = int.MaxValue;
+
+            for(int i = 0; i<arr.Length; i++)
+            {
+                if(arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            return min;
+        }
+
+        public static int[] FindProduct2(int[] arr)
+        {
+            int n = arr.Length;
+            int temp = 1;
+            int i = 1;
+            int[] result = new int[n];
+
+            for(i = 0; i < n; i++)
+            {
+                result[i] = temp;
+                temp *= arr[i];
+            }
+
+            temp = 1;
+
+            for(i = n - 1; i >= 0; i--)
+            {
+                result[i] *= temp;
+                temp *= arr[i];
+            }
+
+            return result;
+        }
+
+        public static int[] FindProduct(int[] arr)
+        {
+            int[] result = new int[arr.Length];
+            int productSum = 1;
+            int currentIndex = 0;
+            for(int i = 0; i<arr.Length; i++)
+            {
+                for(int j = 0; j<arr.Length; j++)
+                {
+                    currentIndex = j;
+
+                    if (currentIndex != i)
+                    {
+                        productSum *= arr[j];
+                        result[i] = productSum;
+                    }
+                }
+                productSum = 1;
+            }
+
+            return result;
+        }
+
+        public static int[] FindSum(int[] arr, int n)
+        {
+            var resultArray = new int[2];
+            if (arr.Length == 0) return arr;
+
+            for(int i = 0; i<arr.Length; i++)
+            {
+                for(int j = i; j<arr.Length; j++)
+                {
+                    if(arr[i] + arr[j] == n)
+                    {
+                        resultArray[0] = arr[i];
+                        resultArray[1] = arr[j];
+                    }
+                }
+            }
+
+            return resultArray;
+        }
+
+        public static int[] MergeArray(int[] arr1, int[] arr2)
+        {
+            var totalLength = arr1.Length + arr2.Length;
+            var arrayResult = new int[totalLength];
+            var index = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arrayResult[index++] = arr1[i];
+            }
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                arrayResult[index++] = arr2[i];
+            }
+
+            Array.Sort(arrayResult);
+
+            return arrayResult;
+        }
+
+        public static int[] MergeArray2(int[] arr1, int[] arr2)
+        {
+            var totalLength = arr1.Length + arr2.Length;
+            var arrayResult = new int[totalLength];
+            var index = 0;
+            var i = 0;
+            var j = 0;
+
+            while (i < arr1.Length && j < arr2.Length)
+            {
+                if (arr1[i] < arr2[j]) {
+                    arrayResult[index++] = arr1[i++];
+                }
+                else
+                {
+                    arrayResult[index++] = arr2[j++];
+                }
+            }
+
+            while (i < arr1.Length)
+                arrayResult[index++] = arr1[i++];
+
+            while (j < arr2.Length)
+                arrayResult[index++] = arr2[j++];
+
+            return arrayResult;
+        }
+
+        //https://www.educative.io/module/lesson/data-structures-in-java/m24OxJp9y4n
+        /// <summary>
+        /// Challenge 1: Remove Even Integers from an Array
+        //  Given an array of size n, remove all even integers from it.Implement this solution in Java and see if it runs without an error.
+        /// </summary>
+        /// <param name="array"></param>
+        public static int[] RemoveEven(int[] array)
+        {
+            var numberOfOddNumbers = 0;
+
+            for(int i = 0; i<array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    numberOfOddNumbers++;
+                }                
+            }
+
+            var arrayToReturn = new int[numberOfOddNumbers];
+            var index = 0;
+
+            for(int i = 0; i<array.Length; i++)
+            {
+                if(array[i] % 2 != 0)
+                {
+                    arrayToReturn[index++] = array[i];
+                }
+            }
+
+            return arrayToReturn;
+        }
+    }
+}
---
+++ EducativeIO/EducativeIO/Program.cs
@@ -0,0 +1,17 @@
+﻿using EducativeArrays;
+using System;
+
+namespace EducativeIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 4, 2, 1, 5, 0 };
+            
+            var result = ArrayChallenges.FindSecondMaximum(arr);
+
+            Console.WriteLine(result);
+        }
+    }
+}
