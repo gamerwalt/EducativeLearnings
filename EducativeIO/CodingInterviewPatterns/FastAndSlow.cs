c345ca8725e760a064d1d8fa9aa21e0cf43141a5
Olawale Adewoyin <gamerwalt@gmail.com>
2021-05-14 14:21:39 -0600

    Added description and solution description

--- EducativeIO/CodingInterviewPatterns/FastAndSlow.cs
+++ EducativeIO/CodingInterviewPatterns/FastAndSlow.cs
@@ -7,8 +7,16 @@ namespace CodingInterviewPatterns
 {
     public class FastAndSlow
     {
+        /// <summary>
+        /// Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
         public static bool HasCycle(ListNode list)
         {
+            ///Solution:
+            ///We should have 2 pointers. One that will move fast (2 nodes at a time) and one that moves slow (1 node at a time)
+            ///at some point, the 2 pointers will meet. Once they meet, we can safely say the linked list has a cycle
             if (list == null || list.next == null) return false;
 
             var fast = list.next;
