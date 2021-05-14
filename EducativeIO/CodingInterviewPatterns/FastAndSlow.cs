8de58f99b6cae0a174b495fb92a6cd6eead90da4
Olawale Adewoyin <gamerwalt@gmail.com>
2021-05-14 15:28:05 -0600

    Solve find length of a linked list cycle

--- EducativeIO/CodingInterviewPatterns/FastAndSlow.cs
+++ EducativeIO/CodingInterviewPatterns/FastAndSlow.cs
@@ -31,5 +31,75 @@ namespace CodingInterviewPatterns
 
             return false;
         }
+
+        /// <summary>
+        /// Given the head of a LinkedList with a cycle, find the length of the cycle.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static int FindLinkedListCycleLength(ListNode list)
+        {
+            ///Solution:
+            ///Just like above, we find if the linkedlist has a cycle, if it does, we calculate the length from there.
+            ///Now, to calculate the length, we use another pointer to go through the loop/cycle again.
+            ///The moment we hit the slow pointer used as the start to calculate the length, that's the current length
+            if (list == null || list.next == null) return -1;
+
+            var fast = list.next; var slow = list.next;
+
+            while(fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if(fast == slow)
+                {
+                    return CalculateLength(slow);
+                }
+            }
+
+            return 0;
+        }
+
+        private static int CalculateLength(ListNode slow)
+        {
+            var current = slow;
+            int cycleLength = 0;
+
+            do
+            {
+                current = current.next;
+                cycleLength++;
+            } while (current != slow);
+
+            /**
+            we can do this also instead of a do while
+            //We increment so we don't have a situation where current is stil = to slow
+            current = current.next;
+            cycleLength++;
+            while (current != slow)
+            {
+                current = current.next;
+                cycleLength++;
+            }
+            **/
+
+            return cycleLength;
+        }
+
+        /// <summary>
+        /// Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle.
+        /// </summary>
+        /// <param name="list"></param>
+        public ListNode FindCycleStart(ListNode list)
+        {
+            ///Solution
+            ///We find the length of the cycle
+            ///if we have a length of 4, We create 2 pointers to start from the beginning of the list
+            ///we move a 1 pointer 4 nodes up
+            ///Then once that's complete, we can then start moving 2 pointers one at a time.
+            ///The moment they meet, that's the cycle start
+            ///
+        }
     }
 }
--- EducativeIO/EducativeIO/Program.cs
+++ EducativeIO/EducativeIO/Program.cs
@@ -17,7 +17,8 @@ namespace EducativeIO
             list.next.next.next = new ListNode(4);
             list.next.next.next.next = new ListNode(5);
             list.next.next.next.next.next = new ListNode(6);
-            Console.WriteLine(FastAndSlow.HasCycle(list));
+            list.next.next.next.next.next.next = list.next.next;
+            Console.WriteLine(FastAndSlow.FindLinkedListCycleLength(list));
 
             list = new ListNode(1);
             list.next = new ListNode(2);
@@ -26,7 +27,7 @@ namespace EducativeIO
             list.next.next.next.next = new ListNode(5);
             list.next.next.next.next.next = new ListNode(6);
             list.next.next.next.next.next.next = list.next.next.next;
-            Console.WriteLine(FastAndSlow.HasCycle(list));
+            Console.WriteLine(FastAndSlow.FindLinkedListCycleLength(list));
         }
     }
 }
