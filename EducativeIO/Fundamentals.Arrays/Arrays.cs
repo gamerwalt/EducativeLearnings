d0660ecf56e1dcc0435ea7b9dc477b6e479bc2ae
Olawale Adewoyin <gamerwalt@gmail.com>
2021-05-15 17:46:49 -0600

    Add arrays for back to basics

---
+++ EducativeIO/Fundamentals.Arrays/Arrays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fundamentals.Arrays
+{
+    /// <summary>
+    /// Lookup - O(n)
+    /// Insert - O(n)
+    /// Delete - O(1) or O(n) (O(n) if you're not deleting at the end of the array)
+    /// </summary>
+    public class Arrays
+    {
+        public int[] Items;
+        public int count;
+
+        public Arrays(int capacity)
+        {
+            Items = new int[capacity];
+        }
+
+        public void Insert(int item)
+        {
+            if (count == Items.Length)
+            {
+                var newItems = new int[count * 2];
+                Array.Copy(Items, newItems, count);
+                Items = newItems;
+            }
+
+            Items[count] = item;
+
+            count++;
+        }
+    }
+}
---
+++ EducativeIO/Fundamentals.OutputConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentals.OutputConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Arrays
+        }
+    }
+}
